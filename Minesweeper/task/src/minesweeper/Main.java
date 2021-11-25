2c7cc9f0bfcb7773da316a780f302caff5de3c84
Dominik <77927514+OverRam@users.noreply.github.com>
2021-11-25 22:02:18 +0100

    Init Minesweeper game

---
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -0,0 +1,8 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper(9);
+        minesweeper.run();
+    }
+}
